0c3008f5df46ae166a519f94f7ee3dbccc89ff70
林水溶 <shuirong1997@icloud.com>
2019-03-30 23:52:23 +0800

    修改activeTabStyle的静态类型检查值为：object

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -17,7 +17,7 @@ const DefaultTabBar = createReactClass({
     tabs: PropTypes.array,
     backgroundColor: PropTypes.string,
     activeTextColor: PropTypes.string,
-    activeTabStyle: PropTypes.string,
+    activeTabStyle: PropTypes.object,
     inactiveTextColor: PropTypes.string,
     textStyle: Text.propTypes.style,
     tabStyle: ViewPropTypes.style,
